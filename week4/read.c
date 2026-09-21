28613344e323e8ef3b0f51413da28131aca8690b
Harshpandey <208502446+harshpandey0308@users.noreply.github.com>
2026-09-21 09:13:09 +0000

    reading file using file descriptor

---
+++ week4/read.c
@@ -0,0 +1,31 @@
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int fd = open("test.txt" , O_RDONLY | O_CREAT , 0444);
+
+    if(fd == -1){
+        perror("file opening failed");
+        exit(EXIT_FAILURE);
+    }
+
+    char msg[274];
+
+    ssize_t success = read(fd , msg , 273);
+
+    if(success == -1){
+        perror("reading the file failed");
+        exit(EXIT_FAILURE);
+    }
+
+    msg[success] = '\0';
+
+    printf("the read message is : %s.\n", msg);
+
+    printf("the offset is %ld.\n", success);
+
+    close(fd);
+}
