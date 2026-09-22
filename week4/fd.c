12326073b52ab9cb0006e798e71b4a5775f5cd8b
Harshpandey <208502446+harshpandey0308@users.noreply.github.com>
2026-09-22 08:28:23 +0000

    offset manipulation and read

--- week4/fd.c
+++ week4/fd.c
@@ -4,72 +4,28 @@
 #include<unistd.h>
 
 int main(){
-int fd = open("test.txt" , O_RDWR | O_CREAT  , 0644);
+    int fd = open("har.txt" , O_WRONLY | O_CREAT , 0666);
 
-if(fd == -1){
-    perror("file open failed");
-    exit(EXIT_FAILURE);
-}
+    if(fd == -1){
+        perror("file opening failed");
+        exit(EXIT_FAILURE);
+    }
 
-printf("the file descriptor is %d.\n", fd);
+    const char msg[13] = "Hello Harsh";
 
-off_t set = lseek(fd , 0 , SEEK_SET);
+    off_t set1 = lseek(fd , 0 , SEEK_SET);
 
-if(set == -1){
-    perror("setting offset failed");
-    exit(EXIT_FAILURE);
-}
-const char msg[] = "Hello file descriptor.\n";
+    printf("the offset is at position %ld\n",set1);
 
-int success = write(fd , msg , sizeof(msg) - 1);
+    ssize_t wr_success = write(fd , msg , sizeof(msg) - 1);
 
-if(success == -1){
-    perror("message sending failed.");
-    exit(EXIT_FAILURE);
-}
+    if(wr_success == -1){
+        perror("message writing failed");
+        exit(EXIT_FAILURE);
+    }
 
-off_t cur = lseek(fd , 3 , SEEK_CUR);
+    close(fd);
 
-if(cur == -1){
-    perror("moving current offset failed");
-    exit(EXIT_FAILURE);
-}
-
-const char msg2[] = "after moving 3 steps far from the word Hello.";
-
-int success1 = write(fd , msg2 , sizeof(msg2) - 1);
-
-if(success1 == -1){
-    perror("moving offset failed");
-    exit(EXIT_FAILURE);
-}
-
-off_t end = lseek(fd , -4 , SEEK_END);
-
-if(end == -1){
-    perror("moving offset from the EOF is failed");
-    exit(EXIT_FAILURE);
-}
-
-const char msg3[] = "I am OPTIMUS PRIME and I am calling all autobots who are refugee in the multiple planet of this galaxy.";
-
-int success3 = write(fd , msg3 , sizeof(msg3) - 1);
-
-if(success3 == -1){
-    perror("seek end failed");
-    exit(EXIT_FAILURE);
-}
-
-off_t size = lseek(fd , 0 , SEEK_END);
-
-if(size == -1){
-    perror("size failed");
-    exit(EXIT_FAILURE);
-}
-
-printf("the file size is %ld", size);
-
-close(fd);
 
 return 0;
 }
--- week4/read.c
+++ week4/read.c
@@ -5,16 +5,16 @@
 #include<stdlib.h>
 
 int main(){
-    int fd = open("test.txt" , O_RDONLY | O_CREAT , 0444);
+    int fd = open("har.txt" , O_RDONLY | O_CREAT , 0444);
 
     if(fd == -1){
         perror("file opening failed");
         exit(EXIT_FAILURE);
     }
 
-    char msg[274];
+    char msg[12];
 
-    ssize_t success = read(fd , msg , 273);
+    ssize_t success = read(fd , msg , 11);
 
     if(success == -1){
         perror("reading the file failed");
